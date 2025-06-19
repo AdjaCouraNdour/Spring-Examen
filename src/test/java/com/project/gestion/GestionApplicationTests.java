77bb43873d83119f7cfa32d67e520a4d2821afcc
Adja Coura Ndour <adja-coura.ndour@ism.edu.sn>
2025-06-19 20:21:15 +0000

    spring

--- src/test/java/com/project/gestion/GestionApplicationTests.java
+++ src/test/java/com/project/gestion/GestionApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GestionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
